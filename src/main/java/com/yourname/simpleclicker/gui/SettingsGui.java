0da84f7fd3634d1c0ec2694e1a39908c07ca6717
Arkasha335 <parovozov2901@gmail.com>
2025-07-08 17:14:26 +0300

    Upgrade mod: Fix GUI and click mechanics

--- src/main/java/com/yourname/simpleclicker/gui/SettingsGui.java
+++ src/main/java/com/yourname/simpleclicker/gui/SettingsGui.java
@@ -3,26 +3,19 @@ package com.yourname.simpleclicker.gui;
 import com.yourname.simpleclicker.config.ModConfig;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
-
 import java.io.IOException;
+import java.awt.Color;
 
 public class SettingsGui extends GuiScreen {
 
-    // ID кнопок для их идентификации
+    // ID кнопок
     private static final int MOD_ENABLED_BUTTON = 0;
     private static final int L_CLICKER_ENABLED_BUTTON = 1;
     private static final int R_CLICKER_ENABLED_BUTTON = 2;
-    
-    private static final int L_CPS_PLUS = 10;
-    private static final int L_CPS_MINUS = 11;
-    private static final int L_RAND_PLUS = 12;
-    private static final int L_RAND_MINUS = 13;
-
-    private static final int R_CPS_PLUS = 20;
-    private static final int R_CPS_MINUS = 21;
-    private static final int R_RAND_PLUS = 22;
-    private static final int R_RAND_MINUS = 23;
-
+    private static final int L_CPS_PLUS = 10, L_CPS_MINUS = 11;
+    private static final int L_RAND_PLUS = 12, L_RAND_MINUS = 13;
+    private static final int R_CPS_PLUS = 20, R_CPS_MINUS = 21;
+    private static final int R_RAND_PLUS = 22, R_RAND_MINUS = 23;
 
     @Override
     public void initGui() {
@@ -30,75 +23,55 @@ public class SettingsGui extends GuiScreen {
         this.buttonList.clear();
 
         int centerX = this.width / 2;
-        int yPos = this.height / 4 - 20;
+        int yPos = this.height / 4;
 
         // --- Глобальные настройки ---
-        this.buttonList.add(new GuiButton(MOD_ENABLED_BUTTON, centerX - 100, yPos, "Mod Enabled: " + (ModConfig.modEnabled ? "ON" : "OFF")));
-        yPos += 24;
-        
+        this.buttonList.add(new GuiButton(MOD_ENABLED_BUTTON, centerX - 100, yPos, 200, 20, "Mod: " + (ModConfig.modEnabled ? "§aON" : "§cOFF")));
+        yPos += 30;
+
+        // --- Колонки для ЛКМ и ПКМ ---
+        int columnLeft = centerX - 155;
+        int columnRight = centerX + 5;
+        int buttonWidthFull = 150;
+        int buttonWidthHalf = 73;
+
         // --- Настройки ЛКМ ---
-        this.buttonList.add(new GuiButton(L_CLICKER_ENABLED_BUTTON, centerX - 155, yPos, 150, 20, "Left Clicker: " + (ModConfig.leftClickerEnabled ? "ON" : "OFF")));
-        yPos += 24;
-        this.buttonList.add(new GuiButton(L_CPS_MINUS, centerX - 155, yPos, 70, 20, "-"));
-        this.buttonList.add(new GuiButton(L_CPS_PLUS, centerX - 85, yPos, 70, 20, "+"));
-        yPos += 24;
-        this.buttonList.add(new GuiButton(L_RAND_MINUS, centerX - 155, yPos, 70, 20, "-"));
-        this.buttonList.add(new GuiButton(L_RAND_PLUS, centerX - 85, yPos, 70, 20, "+"));
-        
-        yPos = this.height / 4 + 28;
+        int yPosLeft = yPos;
+        this.buttonList.add(new GuiButton(L_CLICKER_ENABLED_BUTTON, columnLeft, yPosLeft, buttonWidthFull, 20, "Left Clicker: " + (ModConfig.leftClickerEnabled ? "§aON" : "§cOFF")));
+        yPosLeft += 24;
+        this.buttonList.add(new GuiButton(L_CPS_MINUS, columnLeft, yPosLeft, buttonWidthHalf, 20, "-"));
+        this.buttonList.add(new GuiButton(L_CPS_PLUS, columnLeft + buttonWidthHalf + 4, yPosLeft, buttonWidthHalf, 20, "+"));
+        yPosLeft += 24;
+        this.buttonList.add(new GuiButton(L_RAND_MINUS, columnLeft, yPosLeft, buttonWidthHalf, 20, "-"));
+        this.buttonList.add(new GuiButton(L_RAND_PLUS, columnLeft + buttonWidthHalf + 4, yPosLeft, buttonWidthHalf, 20, "+"));
 
         // --- Настройки ПКМ ---
-        this.buttonList.add(new GuiButton(R_CLICKER_ENABLED_BUTTON, centerX + 5, yPos, 150, 20, "Right Clicker: " + (ModConfig.rightClickerEnabled ? "ON" : "OFF")));
-        yPos += 24;
-        this.buttonList.add(new GuiButton(R_CPS_MINUS, centerX + 5, yPos, 70, 20, "-"));
-        this.buttonList.add(new GuiButton(R_CPS_PLUS, centerX + 75, yPos, 70, 20, "+"));
-        yPos += 24;
-        this.buttonList.add(new GuiButton(R_RAND_MINUS, centerX + 5, yPos, 70, 20, "-"));
-        this.buttonList.add(new GuiButton(R_RAND_PLUS, centerX + 75, yPos, 70, 20, "+"));
-
+        int yPosRight = yPos;
+        this.buttonList.add(new GuiButton(R_CLICKER_ENABLED_BUTTON, columnRight, yPosRight, buttonWidthFull, 20, "Right Clicker: " + (ModConfig.rightClickerEnabled ? "§aON" : "§cOFF")));
+        yPosRight += 24;
+        this.buttonList.add(new GuiButton(R_CPS_MINUS, columnRight, yPosRight, buttonWidthHalf, 20, "-"));
+        this.buttonList.add(new GuiButton(R_CPS_PLUS, columnRight + buttonWidthHalf + 4, yPosRight, buttonWidthHalf, 20, "+"));
+        yPosRight += 24;
+        this.buttonList.add(new GuiButton(R_RAND_MINUS, columnRight, yPosRight, buttonWidthHalf, 20, "-"));
+        this.buttonList.add(new GuiButton(R_RAND_PLUS, columnRight + buttonWidthHalf + 4, yPosRight, buttonWidthHalf, 20, "+"));
     }
 
     @Override
     protected void actionPerformed(GuiButton button) throws IOException {
+        // Логика кнопок остается прежней, здесь ничего не меняем
         switch (button.id) {
-            case MOD_ENABLED_BUTTON:
-                ModConfig.modEnabled = !ModConfig.modEnabled;
-                break;
-            // ЛКМ
-            case L_CLICKER_ENABLED_BUTTON:
-                ModConfig.leftClickerEnabled = !ModConfig.leftClickerEnabled;
-                break;
-            case L_CPS_PLUS:
-                ModConfig.leftCps = Math.min(50.0, ModConfig.leftCps + 0.5); // Ограничиваем макс. CPS
-                break;
-            case L_CPS_MINUS:
-                ModConfig.leftCps = Math.max(1.0, ModConfig.leftCps - 0.5); // Ограничиваем мин. CPS
-                break;
-            case L_RAND_PLUS:
-                ModConfig.leftRandomization = Math.min(1.0, ModConfig.leftRandomization + 0.05); // макс. 100%
-                break;
-            case L_RAND_MINUS:
-                ModConfig.leftRandomization = Math.max(0.0, ModConfig.leftRandomization - 0.05); // мин. 0%
-                break;
-            
-            // ПКМ
-            case R_CLICKER_ENABLED_BUTTON:
-                ModConfig.rightClickerEnabled = !ModConfig.rightClickerEnabled;
-                break;
-            case R_CPS_PLUS:
-                ModConfig.rightCps = Math.min(50.0, ModConfig.rightCps + 0.5);
-                break;
-            case R_CPS_MINUS:
-                ModConfig.rightCps = Math.max(1.0, ModConfig.rightCps - 0.5);
-                break;
-            case R_RAND_PLUS:
-                ModConfig.rightRandomization = Math.min(1.0, ModConfig.rightRandomization + 0.05);
-                break;
-            case R_RAND_MINUS:
-                ModConfig.rightRandomization = Math.max(0.0, ModConfig.rightRandomization - 0.05);
-                break;
+            case MOD_ENABLED_BUTTON: ModConfig.modEnabled = !ModConfig.modEnabled; break;
+            case L_CLICKER_ENABLED_BUTTON: ModConfig.leftClickerEnabled = !ModConfig.leftClickerEnabled; break;
+            case L_CPS_PLUS: ModConfig.leftCps = Math.min(50.0, ModConfig.leftCps + 0.5); break;
+            case L_CPS_MINUS: ModConfig.leftCps = Math.max(1.0, ModConfig.leftCps - 0.5); break;
+            case L_RAND_PLUS: ModConfig.leftRandomization = Math.min(1.0, ModConfig.leftRandomization + 0.05); break;
+            case L_RAND_MINUS: ModConfig.leftRandomization = Math.max(0.0, ModConfig.leftRandomization - 0.05); break;
+            case R_CLICKER_ENABLED_BUTTON: ModConfig.rightClickerEnabled = !ModConfig.rightClickerEnabled; break;
+            case R_CPS_PLUS: ModConfig.rightCps = Math.min(50.0, ModConfig.rightCps + 0.5); break;
+            case R_CPS_MINUS: ModConfig.rightCps = Math.max(1.0, ModConfig.rightCps - 0.5); break;
+            case R_RAND_PLUS: ModConfig.rightRandomization = Math.min(1.0, ModConfig.rightRandomization + 0.05); break;
+            case R_RAND_MINUS: ModConfig.rightRandomization = Math.max(0.0, ModConfig.rightRandomization - 0.05); break;
         }
-        // Перерисовываем GUI, чтобы обновить текст на кнопках
         this.initGui();
     }
 
@@ -108,25 +81,35 @@ public class SettingsGui extends GuiScreen {
         super.drawScreen(mouseX, mouseY, partialTicks);
 
         int centerX = this.width / 2;
-        int yPos = this.height / 4 - 35;
+        int yPos = this.height / 4;
+        int titleColor = Color.WHITE.getRGB();
+        int textColor = Color.LIGHT_GRAY.getRGB();
 
-        this.drawCenteredString(this.fontRendererObj, "AutoClicker Settings", centerX, yPos, 0xFFFFFF);
-        
-        // --- ЛКМ ---
-        yPos = this.height / 4 + 52;
-        this.drawString(this.fontRendererObj, String.format("CPS: %.1f", ModConfig.leftCps), centerX - 155, yPos, 0xFFFFFF);
-        yPos += 24;
-        this.drawString(this.fontRendererObj, String.format("Rand: %.0f%%", ModConfig.leftRandomization * 100), centerX - 155, yPos, 0xFFFFFF);
-        
-        // --- ПКМ ---
-        yPos = this.height / 4 + 52;
-        this.drawString(this.fontRendererObj, String.format("CPS: %.1f", ModConfig.rightCps), centerX + 5, yPos, 0xFFFFFF);
-        yPos += 24;
-        this.drawString(this.fontRendererObj, String.format("Rand: %.0f%%", ModConfig.rightRandomization * 100), centerX + 5, yPos, 0xFFFFFF);
+        this.drawCenteredString(this.fontRendererObj, "AutoClicker Settings", centerX, yPos - 15, titleColor);
+
+        // --- Колонки ---
+        int columnLeft = centerX - 155;
+        int columnRight = centerX + 5;
+        int yPosText = yPos + 54;
+
+        // --- Текст для ЛКМ ---
+        String leftCpsText = String.format("CPS: %.1f", ModConfig.leftCps);
+        this.drawString(this.fontRendererObj, leftCpsText, columnLeft + 2, yPosText, textColor);
+        yPosText += 24;
+        String leftRandText = String.format("Rand: %.0f%%", ModConfig.leftRandomization * 100);
+        this.drawString(this.fontRendererObj, leftRandText, columnLeft + 2, yPosText, textColor);
+
+        // --- Текст для ПКМ ---
+        yPosText = yPos + 54;
+        String rightCpsText = String.format("CPS: %.1f", ModConfig.rightCps);
+        this.drawString(this.fontRendererObj, rightCpsText, columnRight + 2, yPosText, textColor);
+        yPosText += 24;
+        String rightRandText = String.format("Rand: %.0f%%", ModConfig.rightRandomization * 100);
+        this.drawString(this.fontRendererObj, rightRandText, columnRight + 2, yPosText, textColor);
     }
 
     @Override
     public boolean doesGuiPauseGame() {
-        return false; // GUI не ставит игру на паузу
+        return false;
     }
 }
--- src/main/java/com/yourname/simpleclicker/handlers/ClientEventHandler.java
+++ src/main/java/com/yourname/simpleclicker/handlers/ClientEventHandler.java
@@ -4,73 +4,50 @@ import com.yourname.simpleclicker.SimpleClickerMod;
 import com.yourname.simpleclicker.config.ModConfig;
 import com.yourname.simpleclicker.gui.SettingsGui;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
-import java.lang.reflect.Method;
 import java.util.Random;
 
 public class ClientEventHandler {
 
-    private Minecraft mc = Minecraft.getMinecraft();
-    private Random random = new Random();
+    private final Minecraft mc = Minecraft.getMinecraft();
+    private final Random random = new Random();
     private long nextLeftClickTime = 0;
     private long nextRightClickTime = 0;
 
-    // Для доступа к приватным методам Minecraft, мы храним их объекты
-    private static Method clickMouseMethod;
-    private static Method rightClickMouseMethod;
-
-    static {
-        try {
-            // Инициализируем методы через рефлексию.
-            // Имена "func_147116_af" (clickMouse) и "func_147121_ag" (rightClickMouse) актуальны для 1.8.9
-            clickMouseMethod = Minecraft.class.getDeclaredMethod("func_147116_af");
-            rightClickMouseMethod = Minecraft.class.getDeclaredMethod("func_147121_ag");
-            clickMouseMethod.setAccessible(true);
-            rightClickMouseMethod.setAccessible(true);
-        } catch (NoSuchMethodException e) {
-            // Если методы не найдены (например, другая версия игры), выводим ошибку
-            e.printStackTrace();
-        }
-    }
-
     @SubscribeEvent
     public void onKeyInput(KeyInputEvent event) {
-        // Проверяем, была ли нажата наша клавиша для открытия меню
         if (SimpleClickerMod.openSettingsKey.isPressed()) {
-            // Открываем GUI
             mc.displayGuiScreen(new SettingsGui());
         }
     }
 
     @SubscribeEvent
     public void onClientTick(TickEvent.ClientTickEvent event) {
-        // Выполняем логику только в конце тика, чтобы избежать конфликтов
         if (event.phase == TickEvent.Phase.END) {
-            // Проверяем, что игрок в мире, не в меню и мод включен
-            if (mc.thePlayer != null && mc.currentScreen == null && ModConfig.modEnabled) {
-                handleLeftClick();
-                handleRightClick();
+            // Оптимизация: выходим сразу, если мод выключен или мы не в игре
+            if (!ModConfig.modEnabled || mc.thePlayer == null || mc.currentScreen != null) {
+                return;
             }
+            handleLeftClick();
+            handleRightClick();
         }
     }
 
     private void handleLeftClick() {
-        // Проверяем, включен ли кликер для ЛКМ и зажата ли кнопка
         if (ModConfig.leftClickerEnabled && mc.gameSettings.keyBindAttack.isKeyDown()) {
             long currentTime = System.currentTimeMillis();
             if (currentTime >= nextLeftClickTime) {
                 performLeftClick();
-                // Рассчитываем время следующего клика
                 nextLeftClickTime = currentTime + calculateDelay(ModConfig.leftCps, ModConfig.leftRandomization);
             }
         }
     }
 
     private void handleRightClick() {
-        // Аналогичная логика для ПКМ
         if (ModConfig.rightClickerEnabled && mc.gameSettings.keyBindUseItem.isKeyDown()) {
             long currentTime = System.currentTimeMillis();
             if (currentTime >= nextRightClickTime) {
@@ -79,35 +56,32 @@ public class ClientEventHandler {
             }
         }
     }
-    
+
     private long calculateDelay(double cps, double randomization) {
-        if (cps <= 0) return 1000; // Безопасная проверка
+        if (cps <= 0) return 1000;
         double baseDelay = 1000.0 / cps;
-        // (random.nextDouble() - 0.5) дает случайное число от -0.5 до 0.5
-        // Умножаем это на базовую задержку и на коэффициент рандомизации
-        double randomOffset = (random.nextDouble() - 0.5) * baseDelay * randomization;
-        return (long) (baseDelay + randomOffset);
+        if (randomization > 0) {
+            double randomOffset = (random.nextDouble() - 0.5) * baseDelay * randomization;
+            return (long) (baseDelay + randomOffset);
+        }
+        return (long) baseDelay;
     }
-    
+
+    // НОВАЯ, ПРАВИЛЬНАЯ РЕАЛИЗАЦИЯ КЛИКОВ
     private void performLeftClick() {
-        try {
-            // Вызываем приватный метод clickMouse() через рефлексию
-            if (clickMouseMethod != null) {
-                clickMouseMethod.invoke(mc);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        KeyBinding key = mc.gameSettings.keyBindAttack;
+        // Нажимаем клавишу виртуально
+        KeyBinding.setKeyBindState(key.getKeyCode(), true);
+        // Заставляем игру обработать это нажатие
+        KeyBinding.onTick(key.getKeyCode());
+        // Сразу же отпускаем, чтобы симулировать одиночный клик
+        KeyBinding.setKeyBindState(key.getKeyCode(), false);
     }
-    
+
     private void performRightClick() {
-        try {
-            // Вызываем приватный метод rightClickMouse() через рефлексию
-            if (rightClickMouseMethod != null) {
-                rightClickMouseMethod.invoke(mc);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        KeyBinding key = mc.gameSettings.keyBindUseItem;
+        KeyBinding.setKeyBindState(key.getKeyCode(), true);
+        KeyBinding.onTick(key.getKeyCode());
+        KeyBinding.setKeyBindState(key.getKeyCode(), false);
     }
 }
